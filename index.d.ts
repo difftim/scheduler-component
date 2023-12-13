b7ca72d6f7f90c1994ec6e8a22cb47a6db0cbdb0
Brew Dog <baye@stupid.cat>
2023-12-14 02:48:12 +0800

    add ts define

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,29 @@
-import { MyCalendar } from './src/Calendar';
+import React from 'react';
+
+type CalendarRef = {
+  setDate: any;
+  setCurrentView: (view: 'day' | 'week') => any;
+};
+
+type CalendarProps = {
+  events: any[];
+  members: any[];
+  onRenderHeader: (item: any) => JSX.Element;
+  showHeader?: boolean;
+  myInfo: {
+    myID: string;
+    name: string;
+    utcOffset: number;
+  };
+  eventBgColors: any[];
+  onSelectEvent?: any;
+  onSelectSlot?: any;
+};
+
+type CalendarComponent = React.ForwardRefExoticComponent<
+  CalendarProps & React.RefAttributes<CalendarRef>
+>;
+
+declare const MyCalendar: CalendarComponent;
 
 export { MyCalendar };
